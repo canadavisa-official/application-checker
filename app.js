73fe64250c3436a05129aa64ec98322bdf8c6e8f
canadavisa-official <160934829+canadavisa-official@users.noreply.github.com>
2024-02-23 00:39:36 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+        
+        function redirect(){
+            var test =  document.querySelector('#passport');
+          
+                 if(test.value == "A07080315"){
+                     window.location.href="sakib.html"
+                }
+                  else if(test.value == "A00727349"){
+                     window.location.href="aminur.html"
+                }
+                      else if(test.value == "EJ0846527"){
+                     window.location.href="rajib.html"
+                }
+                      else if(test.value == "A07049942"){
+                     window.location.href="rasel3.html"
+                }
+                    else if(test.value == "EL0759340"){
+                     window.location.href="tarek.html"
+                }
+                    else if(test.value == "B00145555"){
+                     window.location.href="selim_rega.html"
+                }
+                   else if(test.value == "A04279673"){
+                     window.location.href="kador.html"
+                }     
+                  else if(test.value == "B00030067"){
+                     window.location.href="mohidul.html"
+                }        
+                  else if(test.value == "A12647090"){
+                     window.location.href="imran_hossain.html"
+                } 
+                  else if(test.value == "EJ0604994"){
+                     window.location.href="mehedi.html"
+                } 
+                 else if(test.value == "EK0596742"){
+                     window.location.href="shahadat.html"
+                }
+                       
+                 else if(test.value == "EF0218614"){
+                     window.location.href="amin.html"
+                } 
+                    else if(test.value == "EH0579097"){
+                     window.location.href="ismail2.html"
+                }  
+                  else if(test.value == "EK0583319"){
+                     window.location.href="hanan.html"
+                }  
+                            else if(test.value == "B00680783"){
+                     window.location.href="sharif.html"
+                }
+                                     else if(test.value == "A11586172"){
+                     window.location.href="abdullah.html"
+                }   
+                                              else if(test.value == "A05166948"){
+                     window.location.href="hasan.html"
+                }      
+                                                                                             
+            else{
+                 alert("invalid Passport")
+            }
+   
+              }
+           
+               
+             
+           
+         
+          
